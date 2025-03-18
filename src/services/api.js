53c7cfeffada1ee0e72623d86daaf9dd8d518b12
NotMustxfa <1591mustafa@gmail.com>
2025-03-18 16:39:48 +0530

    final commit

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const API_KEY = "0b44f8161e06153e392c56e8cfdb15f6";
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`
+    );
+    const data = await response.json();
+    return data.results;
+};
